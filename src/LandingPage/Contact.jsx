86b96696c7a67c19c6bb519cceee3044619b6a1a
Elifsu Tanyeri <sutanyeri@gmail.com>
2023-10-30 14:09:11 +0300

    edit contact and footer

--- src/LandingPage/Contact.jsx
+++ src/LandingPage/Contact.jsx
@@ -1,6 +1,4 @@
 import IconContainer from '../Components/IconContainer';
-import ContactPhone from '../assets/phonepng.png';
-
 import LinkedIn from '../assets/linkedin.svg?react';
 import Mail from '../assets/mail.svg?react';
 import Behance from '../assets/behance.svg?react';
@@ -10,71 +8,58 @@ import Doggie from '../assets/doggie.svg?react';
 const Contact = () => (
     <section
         id='contact'
-        className='flex flex-col items-center relative mb-20 lg:mb-80'
+        className='flex flex-col items-center relative mb-20 lg:mb-40'
     >
-        <span className='p-4 text-[#EEEDD9] rotate-[15deg] lg:rotate-[-25deg] absolute top-1/2 left-[-10%] text-lg lg:text-4xl whitespace-nowrap overflow-hidden w-[400px] sm:w-[900px] lg:w-[1500px]'>
-            User Interface | User Experience | Web | Mobile | Research |
-            Branding | User Flow | Journey Map | User Interface | User
-            Experience | Web | Mobile | Research | Branding |
-        </span>
-
-        <span className='p-4 text-[#EEEDD9] rotate-[-15deg] lg:rotate-[25deg] absolute top-1/2 left-[-10%] text-lg lg:text-4xl whitespace-nowrap overflow-hidden w-[400px] sm:w-[900px] lg:w-[1500px]'>
-            Web | Mobile | Research | Branding | User Flow | Journey Map |
-            Branding | User Experience | Web | Mobile | Research | Branding |
-        </span>
         <h1 className='pb-10'>Contact</h1>
-        <img
-            className='absolute left-[5%] lg:top-[10%] lg:left-[10%] w-[90px] lg:w-[169px]'
-            src={ContactPhone}
-            alt='phone-illustration'
-        />
-        <div className='w-[292px] sm:w-[487px] flex flex-col items-center text-center rounded-full gap-8 z-20 '>
-            <h5 className=' bg-bg p-4 sm:p-7 lg:p-12'>
-                Some platforms you can reach out to me, check on my work, and
-                say me hi!
-            </h5>
-            <div className='flex gap-8'>
-                <IconContainer bgColor={'bg-reddish'}>
-                    <a
-                        target='_blank'
-                        rel='noopener noreferrer'
-                        href='mailto:sutanyeri@gmail.com'
-                    >
-                        <Mail />
-                    </a>
-                </IconContainer>
-                <IconContainer bgColor={'bg-[#3BB3BD]'}>
-                    <a
-                        target='_blank'
-                        rel='noopener noreferrer'
-                        href='https://www.linkedin.com/in/elifsutanyeri/'
-                    >
-                        <LinkedIn />
-                    </a>
-                </IconContainer>
-                <IconContainer bgColor={'bg-yellow'}>
-                    <a
-                        target='_blank'
-                        rel='noopener noreferrer'
-                        href='https://www.behance.net/elifsudesign'
-                    >
-                        <Behance />
-                    </a>
-                </IconContainer>
-                <IconContainer bgColor={'bg-pink'}>
-                    <a
-                        target='_blank'
-                        rel='noopener noreferrer'
-                        href='https://www.dribble.com/elifsudesign'
-                    >
-                        <Dribble />
-                    </a>
-                </IconContainer>
+        <div className='flex relative w-full flex-col-reverse lg:flex-row items-center gap-8 lg:gap-0'>
+            <div className='basis-1/2 w-[292px] sm:w-[487px] flex flex-col items-center justify-center text-center rounded-full gap-8 z-20 md:px-[5%]'>
+                <h5 className=' bg-bg p-4 sm:p-7 lg:p-12'>
+                    Some platforms you can reach out to me, check on my work,
+                    and say me hi!
+                </h5>
+                <div className='flex gap-8'>
+                    <IconContainer bgColor={'bg-reddish'}>
+                        <a
+                            target='_blank'
+                            rel='noopener noreferrer'
+                            href='mailto:sutanyeri@gmail.com'
+                        >
+                            <Mail />
+                        </a>
+                    </IconContainer>
+                    <IconContainer bgColor={'bg-[#3BB3BD]'}>
+                        <a
+                            target='_blank'
+                            rel='noopener noreferrer'
+                            href='https://www.linkedin.com/in/elifsutanyeri/'
+                        >
+                            <LinkedIn />
+                        </a>
+                    </IconContainer>
+                    <IconContainer bgColor={'bg-yellow'}>
+                        <a
+                            target='_blank'
+                            rel='noopener noreferrer'
+                            href='https://www.behance.net/elifsudesign'
+                        >
+                            <Behance />
+                        </a>
+                    </IconContainer>
+                    <IconContainer bgColor={'bg-pink'}>
+                        <a
+                            target='_blank'
+                            rel='noopener noreferrer'
+                            href='https://www.dribble.com/elifsudesign'
+                        >
+                            <Dribble />
+                        </a>
+                    </IconContainer>
+                </div>
             </div>
-            <h2 className='font-Lalezar lg:text-3xl text-lg'>
-                sutanyeri@gmail.com
-            </h2>
-            <Doggie className='doggie-svg' />
+            <div className='basis-1/2 flex flex-col justify-end contact-doggie-wrapper'>
+                <Doggie className='doggie-svg' />
+                <div className='email-wrapper'>sutanyeri@gmail.com</div>
+            </div>
         </div>
     </section>
 );
